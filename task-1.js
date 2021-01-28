12b2c1edd1d768331c314508f849e37efa32efda
Artem Smirnov <aratatem2013@gmail>
2021-01-28 21:35:36 +0200

    вирішив першу задачу

--- task-1.js
+++ task-1.js
@@ -1,4 +1,11 @@
-const titleRef = document.querySelector(".page-title")
-console.dir(titleRef)
+const categoriesRef = document.querySelector("#categories")
+const categoriesQuantity = categoriesRef.children.length
+console.log(`В списке ${categoriesQuantity} категории.`)
 
-titleRef.textContent = "Abracafabra"
+categoriesRef.querySelectorAll(".item").forEach((el) => {
+  const categoryName = el.querySelector("h2").textContent
+  const categoryElementsQuantity = el.querySelector("ul").children.length
+
+  console.log(`Категория:${categoryName}`)
+  console.log(`Количество элементов:${categoryElementsQuantity}`)
+})
