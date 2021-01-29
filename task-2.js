50b5052310237224fa902368e3c4c9aa0344dc1b
Artem Smirnov <aratatem2013@gmail>
2021-01-29 10:29:45 +0200

    виконує другу задачу

--- task-2.js
+++ task-2.js
@@ -1,1 +1,14 @@
 const ingredients = ["Картошка", "Грибы", "Чеснок", "Помидоры", "Зелень", "Приправы"]
+
+const createListItem = (item) => {
+  const listItemRef = document.createElement("li")
+  console.log(listItemRef)
+  listItemRef.textContent = item
+  return listItemRef
+}
+const ingredientsList = ingredients.map((ingredient) => createListItem(ingredient))
+
+console.log(ingredientsList)
+
+const ingredientsListRef = document.querySelector("#ingredients")
+ingredientsListRef.append(...ingredientsList)
