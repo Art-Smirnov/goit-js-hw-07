bf5d453f59ab9d629e6b93e2c10ec8601c421327
Artem Smirnov <aratatem2013@gmail>
2021-02-24 13:35:29 +0200

    фіксить 6 завдання

--- task-6.js
+++ task-6.js
@@ -1,13 +1,12 @@
 const inputRef = document.querySelector('input[data-length="6"]');
-const maxInputLengthRef = inputRef.dataset.length;
+const maxInputLengthRef = Number(inputRef.dataset.length);
 
 inputRef.addEventListener("blur", handleInputLengthCheck);
 inputRef.addEventListener("focus", handleClearClasslist);
 
 function handleInputLengthCheck(e) {
   const inputLength = e.target.value.length;
-
-  inputLength >= maxInputLengthRef ? e.target.classList.add("valid") : e.target.classList.add("invalid");
+  inputLength === maxInputLengthRef ? e.target.classList.add("valid") : e.target.classList.add("invalid");
 }
 
 function handleClearClasslist(e) {
